9e00d7bebc0ccba3f3f9402b130741051f58243f
i4PP <119318152+i4PP@users.noreply.github.com>
2022-12-19 13:20:29 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
+var player = {
+    x: 50,
+    y: 50,
+    zijde: 100,
+    speed: 5,}
+
+
 function setup() {
     createCanvas(1000, 1000);
 }
@@ -5,3 +12,19 @@ function setup() {
 function draw() {
     background("blue");
 }
+
+function   move() {
+    if (keyIsDown(LEFT_ARROW)) {
+      player.x -= player.speed;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      player.x += player.speed;
+    }
+    if (keyIsDown(UP_ARROW)) {
+      player.y -= player.speed;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      player.y += player.speed;
+    }
+    
+  }

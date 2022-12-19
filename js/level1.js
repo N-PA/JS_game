5436cd9d86c51ed20b939a9f3097635f187c2d15
Tiemenbr <tiemenbreu@gmail.com>
2022-12-19 13:34:32 +0100

    class player en weapon toegevoegd

--- js/level1.js
+++ js/level1.js
@@ -1,5 +1,31 @@
+class Weapon {
+
+}
+
+class Player {
+    constructor(sprite) {
+        this.x = 500;
+        this.y = 500;
+        this.sprite = sprite;
+        this.speed = 5;
+    }
+
+    show() {
+        image(this.sprite,this.x,this.y);
+    }
+}
+
+function preload() {
+    weaponImage = loadImage("../images/gun.png");
+    playerImage = loadImage("../images/player.png");
+}
+
 function setup() {
     createCanvas(1000, 1000);
     background("red");
-    
+    player1 = new Player(playerImage);
+}
+
+function draw() {
+    player1.show();
 }

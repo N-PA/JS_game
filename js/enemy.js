ee4f6130ac140abfaaf9ef20b09f091d49f71763
LordDavids <119323479+LordDavids@users.noreply.github.com>
2022-12-21 12:55:27 +0100

    begin aan vijand

--- js/enemy.js
+++ js/enemy.js
@@ -1,4 +1,21 @@
 class vijand {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+        this.sprite = null;
+        this.stapGrootte = null;
+    }
+    beweeg (){ 
+
+        
+        this.x = constrain(this.x,0,canvas.width);
+        this.y = constrain(this.y,0,canvas.height);
+        
+        
+    }
+    
+
+
 
 
 

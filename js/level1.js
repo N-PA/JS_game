d89f842417efba63ff695e6993079566c7e10acb
i4PP <119318152+i4PP@users.noreply.github.com>
2023-01-09 13:12:57 +0100

    Update level1.js

--- js/level1.js
+++ js/level1.js
@@ -38,6 +38,42 @@ class Player {
     }
 }
 
+class Player2 {
+    constructor(sprite) {
+        this.x = 500;
+        this.y = 500;
+        this.sprite = sprite;
+        this.speed = 3;
+    }
+
+    show() {
+        image(this.sprite,this.x,this.y);
+    }
+
+
+    move() {
+        let mvmt = createVector(0, 0);
+
+        if (keyIsDown(65)) {
+            mvmt.x -= this.speed;
+        }
+        if (keyIsDown(68)) {
+            mvmt.x += this.speed;
+        }
+        if (keyIsDown(87)) {
+            mvmt.y -= this.speed;
+        }
+        if (keyIsDown(83)) {
+            mvmt.y += this.speed;
+        }
+
+        mvmt.setMag(this.speed);
+
+        this.x += mvmt.x;
+        this.y += mvmt.y;
+    }
+}
+
 function preload() {
     weaponImage = loadImage("../images/gun.png");
     playerImage = loadImage("../images/player.png");
@@ -46,10 +82,13 @@ function preload() {
 function setup() {
     createCanvas(1000, 1000);
     player1 = new Player(playerImage);
+    player2 = new Player2(playerImage);
 }
 
 function draw() {
     background("red");
     player1.move();
     player1.show();
+    player2.move();
+    player2.show();
 }

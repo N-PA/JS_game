c278dcf94650cc73dcf8f5a94468641e792a4c01
Tiemenbr <tiemenbreu@gmail.com>
2023-02-13 19:23:05 +0100

    eindscherm werkend gemaakt

--- js/begin-eindscherm.js
+++ js/begin-eindscherm.js
@@ -1,7 +1,9 @@
 function level1() {
+    setCookie("score", 0);
     setCookie("scherm", "level1");
 }
 
 function level2() {
+    setCookie("score", 0);
     setCookie("scherm", "level2");
 }
--- js/level1.js
+++ js/level1.js
@@ -9,26 +9,27 @@ let arr = [];
 let start = 10;
 let increment = 10;
 let spawnEnemy = false;
+
 for (let i = 0; i < 10; i++) {
-  arr.push(start + (i * increment));
+    arr.push(start + (i * increment));
 }
 
 
 
-buildings.push({x: 520, y: 110, w: 51, h: 80});
-buildings.push({x: 586, y: 160, w: 80, h: 30});
-buildings.push({x: 280, y: 300, w: 35, h: 45});
-buildings.push({x: 235, y: 280, w: 115 , h: 10});
-buildings.push({x: 107, y: 310, w: 33 , h: 70});
-buildings.push({x: 140, y: 350, w: 70 , h: 30});
-buildings.push({x: 575, y: 265, w: 81 , h: 11});
-buildings.push({x: 60, y: 205, w: 50 , h: 20});
-buildings.push({x: 182, y: 205, w: 120 , h: 20});
-buildings.push({x: 375, y: 205, w: 65 , h: 20});
-buildings.push({x: 420, y: 205, w: 20 , h: 150});
-buildings.push({x: 440, y: 335, w: 80 , h: 20});
-buildings.push({x: 590, y: 335, w: 93 , h: 20});
-buildings.push({x: 755, y: 335, w: 25 , h: 20});
+buildings.push({ x: 520, y: 110, w: 51, h: 80 });
+buildings.push({ x: 586, y: 160, w: 80, h: 30 });
+buildings.push({ x: 280, y: 300, w: 35, h: 45 });
+buildings.push({ x: 235, y: 280, w: 115, h: 10 });
+buildings.push({ x: 107, y: 310, w: 33, h: 70 });
+buildings.push({ x: 140, y: 350, w: 70, h: 30 });
+buildings.push({ x: 575, y: 265, w: 81, h: 11 });
+buildings.push({ x: 60, y: 205, w: 50, h: 20 });
+buildings.push({ x: 182, y: 205, w: 120, h: 20 });
+buildings.push({ x: 375, y: 205, w: 65, h: 20 });
+buildings.push({ x: 420, y: 205, w: 20, h: 150 });
+buildings.push({ x: 440, y: 335, w: 80, h: 20 });
+buildings.push({ x: 590, y: 335, w: 93, h: 20 });
+buildings.push({ x: 755, y: 335, w: 25, h: 20 });
 
 
 
@@ -39,7 +40,7 @@ class Bullet {
         this.angle = angle;
     }
 
-    travel(){
+    travel() {
         this.x += 10 * cos(this.angle);
         this.y += 10 * sin(this.angle);
         push();
@@ -48,32 +49,32 @@ class Bullet {
         translate(this.x, this.y);
         rotate(this.angle);
         imageMode(CENTER);
-        rect(0,-7.5,5,3.75);
+        rect(0, -7.5, 5, 3.75);
         pop();
     }
 }
 
 class Player {
     constructor(sprite) {
-        this.x = 1000/2;
-        this.y = 450/2;
+        this.x = 1000 / 2;
+        this.y = 450 / 2;
         this.sprite = sprite;
         this.speed = 3;
         this.angle = atan2(mouseY - this.y, mouseX - this.x);
     }
 
     show() {
-        const playersizeHeight =  450 * 0.1;
+        const playersizeHeight = 450 * 0.1;
         const playersizewidth = 800 * 0.05;
         push();
         translate(this.x, this.y);
         this.angle = atan2(mouseY - this.y, mouseX - this.x);
         rotate(this.angle);
         imageMode(CENTER);
-        image(this.sprite,0,0,playersizewidth,playersizeHeight);
+        image(this.sprite, 0, 0, playersizewidth, playersizeHeight);
         pop();
-        if (!!bullets.length){
-            for (let i = 0; i < bullets.length; i++){
+        if (!!bullets.length) {
+            for (let i = 0; i < bullets.length; i++) {
                 bullets[i].travel();
                 if (bullets[i].x > canvas.width || bullets[i].x < 0 || bullets[i].y > canvas.height || bullets[i].y < 0) {
                     bullets.splice(i, 1);
@@ -111,12 +112,13 @@ class Player {
         this.x += mvmt.x;
         this.y += mvmt.y;
 
-       
 
-        this.x = constrain(this.x, 20, 780 );
-        this.y = constrain(this.y, 20, 430 );
-        
+
+        this.x = constrain(this.x, 20, 780);
+        this.y = constrain(this.y, 20, 430);
+
     }
+
     checkCollision(buildings) {
         for (let i = 0; i < buildings.length; i++) {
             let building = buildings[i];
@@ -125,119 +127,121 @@ class Player {
                 //  console.log("Collision detected");
                 this.x = this.prevX;
                 this.y = this.prevY;
-
                 break;
             }
         }
     }
-      
-      checkOverlap(a, b) {
+
+    checkOverlap(a, b) {
         return (a.x + 40 > b.x &&
-                a.x < b.x + b.w &&
-                a.y + 45 > b.y &&
-                a.y < b.y + b.h);
-      }
-      bullitHit(vijands) {
+            a.x < b.x + b.w &&
+            a.y + 45 > b.y &&
+            a.y < b.y + b.h);
+    }
+
+    bullitHit(vijands) {
         for (let u = 0; u < bullets.length; u++) {
-          if (dist(bullets[u].x, bullets[u].y, vijands.pos.x, vijands.pos.y) < 15) {
-            bullets.splice(u, 1);
-            return true;
-          }
+            if (dist(bullets[u].x, bullets[u].y, vijands.pos.x, vijands.pos.y) < 15) {
+                bullets.splice(u, 1);
+                return true;
+            }
         }
         return false;
-      }
-     
-      
+    }
 }
 
 class Vijand {
     constructor(sprite) {
         let y;
+
         if (random(1) < 0.5) {
-          y = random(-300, 0);            
-        } else {
-          y = random(height, height + 300);
+            y = random(-300, 0);
         }
+
+        else {
+            y = random(height, height + 300);
+        }
+
         let x = random(-300, width + 300);
         this.pos = createVector(x, y);
         this.pos2 = createVector(x, y);
         this.sprite = sprite;
         this.speed = 1.4;
         this.stop = 0;
-        this.maxDistance = 90; 
+        this.maxDistance = 90;
         this.radius = 60;
         this.collisionRadius = 55;
-        
-    
     }
-    
 
-    draw(){
+    draw() {
         push();
         imageMode(CENTER);
-        image(this.sprite,this.pos.x,this.pos.y,50,50);
+        image(this.sprite, this.pos.x, this.pos.y, 50, 50);
         pop();
     }
 
     update() {
-        this.p = createVector(player1.x,player1.y);
+        this.p = createVector(player1.x, player1.y);
         let diff = p5.Vector.sub(this.p, this.pos);
         diff.limit(this.speed);
         this.pos.add(diff);
     }
+
     wordGeraakt(player1) {
-        if(dist(this.pos.x, this.pos.y, player1.x, player1.y) < 20){
+        if (dist(this.pos.x, this.pos.y, player1.x, player1.y) < 20) {
             return true;
         }
         return false;
-      }
-
-    
-} 
+    }
+}
 
 class VijandGroter {
     constructor(sprite) {
         let y;
         if (random(1) < 0.5) {
-          y = random(-300, 0);            
-        } else {
-          y = random(height, height + 300);
+            y = random(-300, 0);
         }
+
+        else {
+            y = random(height, height + 300);
+        }
+
         let x = random(-300, width + 300);
         this.pos = createVector(x, y);
         this.pos2 = createVector(x, y);
         this.sprite = sprite;
         this.speed = 1.4;
         this.stop = 0;
-        this.maxDistance = 90; 
+        this.maxDistance = 90;
         this.radius = 60;
         this.collisionRadius = 55;
-        
-    
     }
-    draw(){
+
+    draw() {
         push();
         imageMode(CENTER);
-        image(this.sprite,this.pos.x,this.pos.y,50,50);
+        image(this.sprite, this.pos.x, this.pos.y, 50, 50);
         pop();
     }
+
     update() {
-        this.p = createVector(player1.x,player1.y);
+        this.p = createVector(player1.x, player1.y);
         let diff = p5.Vector.sub(this.p, this.pos);
         diff.limit(this.speed);
         this.pos.add(diff);
-    }  
+    }
+
     wordGeraakt(player1) {
-        if(dist(this.pos.x, this.pos.y, player1.x, player1.y) < 20){
+        if (dist(this.pos.x, this.pos.y, player1.x, player1.y) < 20) {
             return true;
         }
         return false;
-      }
-} 
+    }
+}
 
 
 function preload() {
-    map_background =  loadImage("images/level1.png");
+    map_background = loadImage("images/level1.png");
     weaponImage = loadImage("images/gun.png");
     playerImage = loadImage("images/player.png");
     ai = loadImage("images/enemy.png");
@@ -249,8 +253,8 @@ function setup() {
     window.bullets = [];
     player1 = new Player(playerImage, weaponImage);
     frameCount = 0;
-    
 }
+
 function mouseClicked() {
     player1.shoot();
 }
@@ -264,62 +268,61 @@ function draw() {
     push();
     fill("white")
     textFont("Georgia")
-    text('score '+ score,canvas.width - 70, 20 );
+    text('score ' + score, canvas.width - 70, 20);
     pop();
 
     for (let u = vijands.length - 1; u >= 0; u--) {
         vijands[u].draw();
         vijands[u].update();
-    
+
         if (player1.bullitHit(vijands[u])) {
-          vijands.splice(u, 1);
-          score += 1;
-        }
-        if(vijands[u]){
-            if (vijands[u].wordGeraakt(player1)) { // add this
-               noLoop();
-                break;
-            
-            }
+            vijands.splice(u, 1);
+            score += 1;
         }
 
-        
+        if (vijands[u]) {
+            if (vijands[u].wordGeraakt(player1)) {
+                noLoop();
+                setCookie("score", score);
+                window.location.href = "eindscherm.html";
+                break;
+            }
+        }
     }
-    if (framerate >= timer){
+
+    if (framerate >= timer) {
         vijands.push(new Vijand(ai));
         timer *= 0.996;
         x++;
         framerate = 0;
     }
+
     for (let o = enemyBetter.length - 1; o >= 0; o--) {
         enemyBetter[o].draw();
         enemyBetter[o].update();
-    
+
         if (player1.bullitHit(enemyBetter[o])) {
             enemyBetter.splice(o, 1);
-          score += 1;
+            score += 1;
         }
-        if(enemyBetter[o]){
-            if (enemyBetter[o].wordGeraakt(player1)) { // add this
-               noLoop();
+        if (enemyBetter[o]) {
+            if (enemyBetter[o].wordGeraakt(player1)) {
+                noLoop();
+                setCookie("score", score);
+                window.location.href = "eindscherm.html";
                 break;
-            
             }
         }
-        
     }
-      if(enemyBetter == 0)
-        if (score % 10 === 0 && score > 0) {
+
+    if (enemyBetter == 0) //ik snap het nut van deze if statement niet -niemand
+    if (score % 10 === 0 && score > 0) {
         enemyBetter.push(new VijandGroter(ai2))
-    
-        } 
+    }
 
-    
-
-    
     framerate++;
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
 }
 
-    
-   
+
+
